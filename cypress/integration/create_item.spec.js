788eddda4a71acc290706beb28dec47ecd9bb374
rupalivyas <vyas.rupali655@gmail.com>
2021-10-18 12:17:43 +0530

    Corrected test works

--- cypress/integration/create_item.spec.js
+++ cypress/integration/create_item.spec.js
@@ -1,14 +1,14 @@
 describe('creating a todo item', () => {
-    it('Displays the message in the list', () => {
-      cy.visit('http://localhost:3000');
+    it('displays the to do item', () => {
+      cy.visit('../public/index.html');
   
-      cy.get('[data-testid="item"]')
+      cy.get('[data-testid="inputItem"]')
         .type('Walk the dog');
   
       cy.get('[data-testid="addButton"]')
         .click();
   
-      cy.get('[data-testid="item"]')
+      cy.get('[data-testid="inputItem"]')
         .should('have.value', '');
   
       cy.contains('Walk the dog');
